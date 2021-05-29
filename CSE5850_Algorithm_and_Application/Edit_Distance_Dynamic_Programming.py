a9f037ef9cb475f76a91161fe5607801183ab854
gibong <0728na@naver.com>
2021-05-30 01:43:27 +0900

    added string generation function

--- CSE5850_Algorithm_and_Application/Edit_Distance_Dynamic_Programming.py
+++ CSE5850_Algorithm_and_Application/Edit_Distance_Dynamic_Programming.py
@@ -4,18 +4,32 @@
 # Levenshtein distance
 # todo: build code to compare time complexity among approaches
 
+import random
+import string
+import matplotlib.pyplot as plt
+
 class LevDistance(object):
     '''
         computes string similarity, based on the minimum edit counts of change from one string to the other.
     '''
-    def __init__(self, str1, str2):
+    def __init__(self, str1=None, str2=None, M=None, N=None):
         self.str1 = str1
         self.str2 = str2
+        self.M = M
+        self.N = N
+
+    def randStr(self, chars = string.ascii_lowercase, N = 10):
+        return ''.join(random.choice(chars) for _ in range(N))
 
     def dist(self, mode='DP'):
         '''
             choose approach between DP and Brute-force, for computing Levenshtein Distance
         '''
+        if self.str1 is None:
+            self.str1 = self.randStr(N=self.M)
+        if self.str2 is None:
+            self.str2 = self.randStr(N=self.N)
+
         if mode == 'BF':
             return self.bf_(self.str1, self.str2)
 
@@ -58,6 +72,8 @@ class LevDistance(object):
                        self.bf_(str1[:-1], str2[:-1])
                        )
 
+    def plot_(self):
+
 if __name__ == '__main__':
-    EditDist = LevDistance(str1='kitten', str2='sitting')
+    EditDist = LevDistance(M=10, N=10) # assign valid strings, or integers for randomly generated strings
     print(EditDist.dist())
